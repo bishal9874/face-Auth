d2fd98a38506b0476d2a3529392a74a577a706d7
bishal9874 <bm8130377@gmail.com>
2023-01-10 12:29:51 +0530

    hh

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:camera/camera.dart';
+import 'package:face_auth_flutter/onboard/onboard.dart';
 import 'package:face_auth_flutter/page/face_recognition/camera_page.dart';
 import 'package:face_auth_flutter/page/login_page.dart';
 import 'package:face_auth_flutter/utils/local_db.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
+// int? isviewed;
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   cameras = await availableCameras();
@@ -15,7 +17,12 @@ Future main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  // isviewed = prefs.getInt('onBoard');
   runApp(const MyApp());
 }
 
@@ -23,9 +30,9 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(
+  Widget build(BuildContext context) =>  MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: "Face Auth",
-        home: LoginPage(),
+        title: "Smart Ration",
+        home:OnBoard() 
       );
 }
--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:face_auth_flutter/page/face_recognition/camera_page.dart';
 import 'package:face_auth_flutter/utils/local_db.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../utils/utils.dart';
 
@@ -20,40 +21,56 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text("Face Authentication"),
-          centerTitle: true,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(32),
-          child: Center(
-            child: Column(
+        // appBar: AppBar(
+        //   title: const Text("Face Authentication"),
+        //   centerTitle: true,
+        // ),
+        backgroundColor: Color.fromARGB(255, 241, 220, 220),
+
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(32),
+            child: Center(
+              child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                buildButton(
-                  text: 'Register',
-                  icon: Icons.app_registration_rounded,
-                  onClicked: () async {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const FaceScanScreen()));
-                  },
-                ),
-                const SizedBox(height: 24),
-                buildButton(
-                  text: 'Login',
-                  icon: Icons.login,
-                  onClicked: () async {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => FaceScanScreen(
-                                  user: LocalDB.getUser(),
-                                )));
-                  },
-                ),
-              ],
+                children: [
+                  Text("Welcome to Smart Ration App ",
+                  style: GoogleFonts.raleway(
+                    fontWeight:FontWeight.bold,
+                    fontSize: 24,
+                    ),
+                  ),
+                  // Spacer(),
+                  const SizedBox(height: 20),
+                  Container(
+                    child:Image.asset("assets/imageOB-removebg-preview.png")
+                  ),
+                  const SizedBox(height: 10),
+                  buildButton(
+                    text: 'Register',
+                    //icon: Icons.app_registration_rounded,
+                    onClicked: () async {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const FaceScanScreen()));
+                    },
+                  ),
+                  const SizedBox(height: 24),
+                  buildButton(
+                    text: 'Login',
+                   // icon: Icons.login,
+                    onClicked: () async {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => FaceScanScreen(
+                                    user: LocalDB.getUser(),
+                                  )));
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -61,18 +78,34 @@ class _LoginPageState extends State<LoginPage> {
 
   Widget buildButton({
     required String text,
-    required IconData icon,
+    // required IconData icon,
     required VoidCallback onClicked,
-  }) =>
-      ElevatedButton.icon(
-        style: ElevatedButton.styleFrom(
-          minimumSize: const Size.fromHeight(50),
-        ),
-        icon: Icon(icon, size: 26),
-        label: Text(
-          text,
-          style: const TextStyle(fontSize: 20),
-        ),
-        onPressed: onClicked,
-      );
+  }) =>InkWell(
+    onTap: onClicked,
+    child: Padding(
+      padding:const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+        color:  Colors.deepPurple,
+        borderRadius: BorderRadius.circular(12)),
+        child: Center(child: Text(text,style: GoogleFonts.raleway(
+          fontSize: 18.0,
+          color: Colors.white,
+          fontWeight: FontWeight.bold
+        ),)),
+      )
+    ),
+  );
+      // ElevatedButton.icon(
+      //   style: ElevatedButton.styleFrom(
+      //     minimumSize: const Size.fromHeight(50),
+      //   ),
+      //   icon: Icon(icon, size: 26),
+      //   label: Text(
+      //     text,
+      //     style: const TextStyle(fontSize: 20),
+      //   ),
+      //   onPressed: onClicked,
+      // );
 }
